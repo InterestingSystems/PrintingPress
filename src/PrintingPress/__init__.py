e5318eef7540c7cda66c9fb4b186239cc0b630e6
hysrx <rxyth@criptext.com>
2021-01-03 04:03:44 +0800

    fix this ???

--- src/PrintingPress/__init__.py
+++ src/PrintingPress/__init__.py
@@ -1,3 +1,3 @@
-from .placements import Placements as Placement  # noqa: F401
+from .placements import Placements  # noqa: F401
 from .printingpress import operate  # noqa: F401
 from . import exceptions  # noqa: F401
