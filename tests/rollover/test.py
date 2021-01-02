e0d3d188514e183788f587ba455a8be8110abebc
hysrx <rxyth@criptext.com>
2021-01-02 23:46:56 +0800

    Update tests

--- tests/rollover/test.py
+++ tests/rollover/test.py
@@ -1,24 +1,26 @@
-print("rollover test")
-
-from src import PrintingPress as pp
+from src.PrintingPress import printingpress, placements
 from PIL import Image
 
 
 img = Image.new(mode="RGBA", size=(4000, 4000))
 
-places = pp.Placements.parse(
+places = placements.Placements.parse(
     {
         "rollover": {
             "type": "text",
             "path": "tests/Manrope.ttf",
-            "text": "You, say, isn't it hard? Paddling out, paddling out?, g",
-            "xy": [0, 0],
-            "wh": [4000, 4000],
-            "font_size": 700,
+            "text": "What Goes Up Must Come Down",
+            "xy": [100, 100],
+            "wh": [3800, 3800],
+            "font_size": 1000,
             "fit": True,
             "font_variant": "Bold",
+            "font_colour": [0, 255, 255],
+            "font_opacity": 255,
+            "bg_colour": [255, 0, 255],
+            "bg_opacity": 128,
         }
     }
 )
 
-pp.operate(image=img, placements=places).save("tests/rollover/output.png")
+printingpress.operate(image=img, placements=places).save("tests/rollover/output.png")
